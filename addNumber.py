543ce430e62e2cf68698d7806469e89a0de742f8
Renato R. Resende <resenderr@gmail.com>
2019-11-25 15:51:48 -0300

    added the soluition for addNumber

---
+++ addNumber.py
@@ -0,0 +1,44 @@
+def increment(number):
+    carry = 0
+    for i in range(len(number)-1, -1, -1):
+        v = number[i] + 1
+        if v >= 10:
+            carry = 1
+        else:
+            carry = 0    
+        number[i] = v % 10
+        if carry == 0:
+            return number
+
+    if carry == 1:
+        result = [0] * (len(number) + 1)
+        result[0] = carry
+        return result
+
+
+data = [9,9,8]
+print(data)
+print(increment(data))
+print('\n')
+
+data = [9,9,9]
+print(data)
+print(increment(data))
+print('\n')
+
+
+data = [2,4,5]
+print(data)
+print(increment(data))
+print('\n')
+
+
+data = [2,4,9]
+print(data)
+print(increment(data))
+print('\n')
+
+data = [2,9,9]
+print(data)
+print(increment(data))
+print('\n')
